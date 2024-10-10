61ac80e8d252e8b4c6cfaa7a5b5707595875bfb0
Cu-Ba <raulsaints@hotmail.com>
2024-10-10 20:18:38 +0200

    Update
    Aqui intento hacer que el escenario avance hacia el jugador y se instancie justo detras para hacer un loop infinito

--- Assets/Scripts/InstanciarCalle.cs
+++ Assets/Scripts/InstanciarCalle.cs
@@ -1,32 +1,48 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class InstanciarCalle : MonoBehaviour
-//{
+public class InstanciarCalle : MonoBehaviour
+{
+    //De aqui sacare las instancias de la calle asi como la posicion del jugador
+    public GameObject callePrefab;
+    public Transform player;
 
-//    PlayerManager playerManager;
+    //Longitud de la seccion del terreno
+    float calleLenght = 5f;
 
-//    [SerializeField] GameObject escenarioPrefab;
+    Vector3 nextSpawmPosition;
 
-//    //float speed = 20f;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        playerManager = GameObject.Find("NavePrefab").GetComponent<PlayerManager>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSpawmPosition = transform.position + new Vector3(0, 0, calleLenght);          //Ojo, segun entiendo, lo que le digo aqui es vete 30 metros en Z hacia atras (que es lo que mide el terreno) y generate
+    }                                                                                     //con lo que deberia generarse exactamente detras del terreno generado, creando un loop infinito de terrenos !!EN TEORIA!!
 
-//    }
+    // Update is called once per frame
+    void Update()
+    {
+        ComprobarPosicionJugador();
+    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        speed = playerManager.speed;
-//        transform.Translate(Vector3.back * Time.deltaTime * speed);
-//        if (transform.position.z <= -200f)
-//        {
+    void ComprobarPosicionJugador()
+    {
+        Debug.Log("Player Z position: " + player.position.z);
+        Debug.Log("Next Spawm Position: " + nextSpawmPosition.z);
 
-//            Instantiate(escenarioPrefab, new Vector3(0f, 0f, 400f), Quaternion.identity);
-//            Destroy(gameObject);
-//        }
-//    }
-//}
+        if (player.position.z >= nextSpawmPosition.z - calleLenght)
+        {
+            SpawmNextSection();
+            
+        }
+    }
+
+    void SpawmNextSection()
+    {
+        Debug.Log("Nuevo escenario...");
+        
+        Instantiate(callePrefab, nextSpawmPosition, Quaternion.identity);
+
+        nextSpawmPosition += new Vector3(0, 0, calleLenght);
+    }
+}
